dafd4bfadb015aba55ced60781ab5bf590ee227b
Carl Jacob Cruz <40680430+JacobDiazCruz@users.noreply.github.com>
2019-11-10 14:44:06 +0800

    Update Register.js

--- client/src/components/auth/Register/Register.js
+++ client/src/components/auth/Register/Register.js
@@ -7,31 +7,41 @@ import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import LandingLogo from '../../../images/logo.png';
 
+// forms
+import * as Yup from "yup";
+import { Formik, Form, Field } from "formik";
+
+// Yup
+const validationSchema = Yup.object().shape({
+	name: Yup.string().min(8, "Must be 8 characters long").required(),
+	email: Yup.string().email("Must be a valid email").required()
+})
+
 const Register = ({ setAlert, register, isAuthenticated }) => {
-	const [formData, setFormData] = useState({
-		name: '',
-		email: '',
-		password: '',
-		confirmpassword: ''
-	});
+	// const [formData, setFormData] = useState({
+	// 	name: '',
+	// 	email: '',
+	// 	password: '',
+	// 	confirmpassword: ''
+	// });
 
-	const { name, email, password, confirmpassword } = formData;
+	// const { name, email, password, confirmpassword } = formData;
 
-	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+	// const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	const onSubmit = async e => {
-		e.preventDefault();
-		if(password !== confirmpassword) {
-			setAlert('password didnt match', 'danger');
-		} else {
-			register({ name, email, password });
-		}
-	}
+	// const onSubmit = async e => {
+	// 	e.preventDefault();
+	// 	if(password !== confirmpassword) {
+	// 		setAlert('password didnt match', 'danger');
+	// 	} else {
+	// 		register({ name, email, password });
+	// 	}
+	// }
 
 	//Redirect if logged in
-	if(isAuthenticated) {
-		return <Redirect to="/posts" />
-	}
+	// if(isAuthenticated) {
+	// 	return <Redirect to="/posts" />
+	// }
 
 	return (
 		<div className="register-page">
@@ -43,45 +53,63 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
 			</div>
 			<div className="register-col-2">
 				<h1 className="register-title">Sign Up Now</h1>
-				<form className="register-form" onSubmit={e => onSubmit(e)}>
-					<input 
-						type="text" 
-						placeholder="Username"
-						name="name"
-						value={name}
-						className="register-input"
-						onChange={e => onChange(e)} 
-					/>
-					<br/>
-					<input 
-						type="email" 
-						placeholder="Email Address" 
-						name="email" 
-						value={email}
-						className="register-input"
-						onChange={e => onChange(e)}  
-					/>
-					<br/>
-					<input 
-						type="password" 
-						placeholder="Password" 
-						name="password" 
-						value={password}
-						className="register-input"
-						onChange={e => onChange(e)}  
-					/>
-					<br/>
-					<input 
-						type="password" 
-						placeholder="Confirm Password" 
-						name="confirmpassword" 
-						value={confirmpassword}
-						className="register-input"
-						onChange={e => onChange(e)}  
-					/>
-					<button className="register-btn">Sign Up</button>
-					<Link to="/login"><button className="register-login-btn">Sign In</button></Link>
-				</form>
+				<Formik 
+					initialValues={{name: '', email: '', password: '', confirmpassword: ''}}
+					onSubmit={({setSubmitting, name, email, password}) => {
+						register({name, email, password});
+
+						if(isAuthenticated) {
+							return <Redirect to="/posts" />
+						}
+					}}
+				>
+					{({ values, errors, touched, handleChange, handleBlur, isSubmitting}) => (
+						<Form className="register-form">
+							<Field 
+								type="text"
+								placeholder="Username"
+								name="name"
+								value={values.name}
+								className={touched.name && errors.name ? "has-error" : null}
+								onBlur={handleBlur}
+								onChange={handleChange} 
+							/>
+							<br/>
+							<Field 
+								type="email" 
+								placeholder="Email Address" 
+								name="email" 
+								value={values.email}
+								className={touched.email && errors.email ? "has-error" : null}
+								onBlur={handleBlur}
+								onChange={handleChange}  
+							/>
+							<br/>
+							<Field 
+								type="password" 
+								placeholder="Password" 
+								name="password" 
+								value={values.password}
+								className={touched.password && errors.password ? "has-error" : null}
+								onBlur={handleBlur}
+								onChange={handleChange} 
+							/>
+							<br/>
+							<Field 
+								type="password" 
+								placeholder="Confirm Password" 
+								name="confirmpassword" 
+								value={values.confirmpassword}
+								className={touched.confirmpassword && errors.confirmpassword ? "has-error" : null}
+								onBlur={handleBlur}
+								onChange={handleChange}  
+							/>
+							<button className="register-btn" disabled={isSubmitting}>Sign Up</button>
+							{isSubmitting ? "please wait" : "submit"}
+							<Link to="/login"><button className="register-login-btn">Sign In</button></Link>
+						</Form>
+					)}
+				</Formik>
 			</div>
 		</div>
 	)
@@ -96,6 +124,5 @@ Register.propTypes = {
 const mapStateToProps = state => ({
 	isAuthenticated: state.auth.isAuthenticated
 });
-
 
 export default connect(mapStateToProps, { setAlert, register })(Register);
